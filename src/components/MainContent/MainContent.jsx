dba4228409947c1dc42fcd6030fcb53a19d324ce
KanekarSnehal <snehalkanekar43@gmail.com>
2022-03-27 12:43:40 +0530

    removed comments

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -3,16 +3,6 @@ import { NoteInput, PinnedNotes, OtherNotes } from "../index";
 export const MainContent = () => {
   return (
     <div className="main-content-container">
-      {/* <label for="searchbar">
-        <input
-          class="input-corner-rounded input-md"
-          type="text"
-          name="searchbar"
-          id="searchbar"
-          placeholder="Search..."
-        />
-      </label> */}
-
       <NoteInput />
       <PinnedNotes />
       <OtherNotes />
