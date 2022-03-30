5285b7d10fecb8fd5c8d3ff01bf811aaaef41d0d
KanekarSnehal <snehalkanekar43@gmail.com>
2022-03-30 17:25:28 +0530

    added update label feature on lables apge

---
+++ src/components/UpdateLabels/UpdateLabels.jsx
@@ -0,0 +1,23 @@
+import "./updateLabels.css";
+
+export const UpdateLabels = () => {
+  const labelArr = ["class", "work", "study"];
+  return (
+    <fieldset className="py-16 px-32 text-center">
+      <legend>Update Labels</legend>
+      {labelArr.map((labelItem) => (
+        <span className="mx-8 label">
+          {labelItem}
+          <i className="fa-solid fa-xmark  icon"></i>
+        </span>
+      ))}
+      <div>
+        <input
+          type="text"
+          className="input-md input-round my-16"
+          placeholder="Enter label you want to add"
+        />
+      </div>
+    </fieldset>
+  );
+};
