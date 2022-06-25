9b94c91d7e4a2f71a7c2d4c17103137ef0def2d2
KanekarSnehal <snehalkanekar43@gmail.com>
2022-06-25 20:04:33 +0530

    fixed token issue

--- src/services/noteServices.jsx
+++ src/services/noteServices.jsx
@@ -1,30 +1,30 @@
 import axios from "axios";
 
-const config = {
+const getConfig = () => ({
   headers: { authorization: localStorage.getItem("magnetNoteToken") },
-};
+});
 const notesUrl = "/api/notes";
 const archiveUrl = "/api/notes/archives";
 
-export const getNotes = async () => await axios.get(`${notesUrl}`, config);
+export const getNotes = async () => await axios.get(`${notesUrl}`, getConfig());
 
 export const addNote = async (note) =>
-  await axios.post(`${notesUrl}`, { note }, config);
+  await axios.post(`${notesUrl}`, { note }, getConfig());
 
 export const updateNote = async (noteID, note) =>
-  await axios.post(`${notesUrl}/${noteID}`, { note }, config);
+  await axios.post(`${notesUrl}/${noteID}`, { note }, getConfig());
 
 export const deleteNote = async (noteID) =>
-  await axios.delete(`${notesUrl}/${noteID}`, config);
+  await axios.delete(`${notesUrl}/${noteID}`, getConfig());
 
 export const getArchivedNotes = async () =>
   await axios.get(`${archiveUrl}`, config);
 
 export const addToArchivedNote = async (note) =>
-  await axios.post(`${archiveUrl}/${note._id}`, { note }, config);
+  await axios.post(`${archiveUrl}/${note._id}`, { note }, getConfig());
 
 export const restoreFromArchivedNote = async (noteId) =>
-  await axios.post(`api/archives/restore/${noteId}`, {}, config);
+  await axios.post(`api/archives/restore/${noteId}`, {}, getConfig());
 
 export const deleteFromArchivedNote = async (noteId) =>
-  await axios.delete(`api/archives/delete/${noteId}`, config);
+  await axios.delete(`api/archives/delete/${noteId}`, getConfig());
